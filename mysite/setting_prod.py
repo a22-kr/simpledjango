35140dd6bed1b15a3616c163eb318cdadff17985
Aidar <a.abdyrahmanov99@gmail.com>
2018-03-20 21:03:37 +0600

    Fix errors

--- mysite/setting_prod.py
+++ mysite/setting_prod.py
@@ -1,1 +1,12 @@
-setting_prod.py
+from .settings import *
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'simpledjango',
+       'USER': 'simpledjangouser',
+       'PASSWORD': '123simpledjango123',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
+}
